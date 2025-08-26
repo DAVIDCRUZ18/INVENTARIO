3e660e3dc72beb525960bd10bd444f0f36a7aeed
Tecnología CCCargas <c.tecnologia@cccargas.com>
2025-08-26 16:57:31 -0500

    pagina de inventario ya rreglo de botones

--- Javascrip/dashboard.js
+++ Javascrip/dashboard.js
@@ -1,6 +1,6 @@
-function irPagina() {
-    window.location.href = "/html/dashboard.html";
+function IrPrincipal() {
+    window.location.href = "/index.html";
 }
-function irPagina() {
-    window.location.href = "/html/dashboard.html";
+function Ingresar() {
+    window.location.href = "/html/inventario.html";
 }
--- Javascrip/index.js
+++ Javascrip/index.js
@@ -1,6 +1,6 @@
 function irPagina() {
     window.location.href = "/html/dashboard.html";
 }
-function irPagina() {
-    window.location.href = "/html/dashboard.html";
+function IrPrincipal() {
+    window.location.href = "/index.html";
 }
